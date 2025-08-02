b1d00548e516e87b36c9a4040f4fb5261a8e12d5
yasmeen_alqady <yasmeenalqady117@gmail.com>
2025-08-03 00:41:19 +0300

    add username page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/dashboard/',
   plugins: [
     tailwindcss(),
     vue(),
